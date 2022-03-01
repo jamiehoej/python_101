434c25b724c48e76dc63c6943e6abf4b070fe8c3
Jamie Høj <jamiehoej@gmail.com>
2022-03-01 21:38:46 +0100

    added plot

--- terminal_velocity.py
+++ terminal_velocity.py
@@ -1,4 +1,7 @@
 from math import sqrt,tanh
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 # Formula solve F=1/2*c*Rho*A*v^2
 # F= force, c= drag coefficient, rho = fluid density, v = velocity
@@ -16,6 +19,29 @@ def v(t, C_d=0.1):
 
 	return vt
 
+# testing matplotlib
+def plot():
+	x=np.linspace(0,2*np.pi,200)
+	y=np.sin(x)
+
+	fig = plt.figure()
+	plt.plot(x, y)
+	plt.show()
+
+def plot_vt():
+	t=np.linspace(0,20,200) # create list from 0 to 20
+	y=[v(i) for i in t]
+	# print(y)
+	fig = plt.figure()
+	plt.plot(t, y)
+	plt.xlabel("time [s]")
+	plt.ylabel("velocity [m/s]")
+	plt.show()
+
+
 # function testing
 print(v(t=2, C_d=0.01))
 print(v(t=2))
+# plot()
+plot_vt()
+
