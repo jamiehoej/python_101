fe7ea79febdf3ad901b0f7c592937db0fe945b1a
Jamie Høj <jamiehoej@gmail.com>
2022-03-03 23:23:07 +0100

    learning if or elif

--- playing_with_python.py
+++ playing_with_python.py
@@ -9,3 +9,16 @@ msg=aboutme("martin",26,"odense")
 print(msg)
 
 print(aboutme("1111",23442,"dkjscanvjdkasnv"))
+
+def aboutme2(name: str, age: int, city: str) -> str:
+	word=""
+	if age<=1:
+		word=" year"
+	elif age>1:
+		word=" years"
+	
+	s="hello my name is " + name + " I am " + str(age) + word + " old " + "i live in " + city
+	return s
+
+# we run this other function, yes hehe
+print(aboutme2("boris",2,"kentucky"))
