198486d4766b3628a488040a44b9067110c4852b
Jamie Høj <jamiehoej@gmail.com>
2022-03-03 22:56:32 +0100

    going through the basics

---
+++ playing_with_python.py
@@ -0,0 +1,16 @@
+from re import S
+
+
+def aboutme(name: str, age: int, city: str) -> str:
+	s="hello my name is " + name + " I am " + str(age) + " old " + "i live in " + city
+
+	return s
+
+
+# now we call the function
+msg=aboutme("martin",26,"odense")
+print(msg)
+
+print(aboutme("1111",23442,"dkjscanvjdkasnv"))
+
+
